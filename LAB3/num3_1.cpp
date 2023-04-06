47e2f9123e4ffd73654cdd6baf2cb256e4248426
ZhumaevaVika <s25781256@gmail.com>
2023-04-06 12:16:55 +0300

    LAB3 num3_1 fix

--- LAB3/num3_1.cpp
+++ LAB3/num3_1.cpp
@@ -29,7 +29,7 @@ void get_random_speed(Object(&objects)[1], int n){
     srand(time(0));
     for (int k=0; k<n; k++){
         if (objects[k].key != 2){
-            int q = RandInt(0, 1);
+            int q = rand() % 3 - 1;
                     if (q==0){
                         objects[k].vi = 1;
                     }
@@ -70,13 +70,11 @@ void generate_arr(int real_size, int n, Object(&objects)[1]){
 }
 
 void move(int real_size, int n, Object(&objects)[1]){
-    for (int i=0; i<n; i++){
-        objects[i].i += objects[i].vi;
-        if ((objects[i].i == real_size-1)or(objects[i].i == 0)){
-                objects[i].key = 2;
-                objects[i].vi = 0;
+        objects[0].i += objects[0].vi;
+        if ((objects[0].i == real_size-1)or(objects[0].i == 0)){
+                objects[0].key = 2;
+                objects[0].vi = 0;
             }
-    }
     get_random_speed(objects, n);
 }
 
@@ -93,7 +91,7 @@ int main(){
         if (real_size == 10000){
             step = 1000;
         }
-
+        int counter = 0;
         auto begin = std::chrono::steady_clock::now();
         for(unsigned cnt = 1000; cnt != 0 ; --cnt){
             generate_arr(real_size, num_obj, objects);
@@ -104,12 +102,13 @@ int main(){
                     flag += objects[i].key%2;
                 }
                 move(real_size, num_obj, objects);
+                counter++;
             }
         }
         auto end = std::chrono::steady_clock::now();
         auto time_span =
         std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-        cout << real_size << ' ' << time_span.count() << endl;
+        cout << real_size << ' ' << counter/10 << endl;
         
     }
     fclose (stdout);
